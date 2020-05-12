8268d45b72fa19b2065147be0aacbb85d7e27f6b
Xing Rong Tey <e0415517@u.nus.edu>
2020-05-12 19:21:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/conveproapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.conveproapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
